24967849caa32e72df8909a816b186f4b78b85bd
YuyoungRhee <bbclara@naver.com>
2024-11-29 09:55:00 +0900

    fix(Util): 각 요소의 공백을 제거하도록 수정
    - Menu를 처음 초기화할 때 공백까지 같이 메뉴명으로 들어가는 현상으로 인해 오류 발생하여 수정함

--- src/main/java/menu/util/Util.java
+++ src/main/java/menu/util/Util.java
@@ -3,13 +3,22 @@ package menu.util;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Util {
     public static final String NULL_OR_BLANK_INPUT = "[ERROR] 입력값은 비어있거나 공백일 수 없습니다.";
 
     public static List<String> parseInputByComma(String input) {
         validateNullOrBlank(input);
-        return Arrays.asList(input.split(","));
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> parseInputByCommaCanNull(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     private static void validateNullOrBlank(String input) {
